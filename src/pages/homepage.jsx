2b811262bb78a0fafd89ad7e10568d9258ee12f9
Yohan <125958519+Yohanskie101@users.noreply.github.com>
2025-02-23 08:29:01 +0800

    NewBg

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom'; // Import Link
+import HomeBG from '../assets/HomeBG.mp4';
 
 function Homepage() {
-  return (
-    <div>
-      <h1>This is the Homepage</h1>
-      <p>Go to the <Link to="/flower">Flower Page</Link>.</p>
-    </div>
-  );
-}
-
-export default Homepage;
+    return (
+      <div className="homepage-container">
+        {/* Video container */}
+        <div className="video-background">
+          <video autoPlay loop muted>
+            <source src={HomeBG} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        </div>
+  
+        {/* Content */}
+        <div className="content">
+          <h1>This is the Homepage</h1>
+          <p>
+            Go to the <Link to="/flower">Flower Page</Link>.
+          </p>
+        </div>
+      </div>
+    );
+  }
+  
+  export default Homepage;
