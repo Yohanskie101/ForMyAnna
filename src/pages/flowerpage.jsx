6170b61f4b51c912eada94c57efd087fc0d58b89
Yohan <125958519+Yohanskie101@users.noreply.github.com>
2025-02-23 18:57:42 +0800

    New Daisyui

--- src/pages/flowerpage.jsx
+++ src/pages/flowerpage.jsx
@@ -29,6 +29,26 @@ function FlowerPage() {
   return (
     <div className="flower-page">
       <div className="night"></div>
+
+      <button
+        className="btn absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-36"
+        onClick={() => document.getElementById("my_modal_3").showModal()}
+      >
+        Click Me!
+      </button>
+      <dialog id="my_modal_3" className="modal">
+        <div className="modal-box">
+          <form method="dialog">
+            {/* if there is a button in form, it will close the modal */}
+            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+              ✕
+            </button>
+          </form>
+          <h3 className="font-bold text-lg">Hello!</h3>
+          <p className="py-4">Press ESC key or click on ✕ button to close</p>
+        </div>
+      </dialog>
+
       <div className="flowers">
         <div className="flower flower--1">
           <div className="flower__leafs flower__leafs--1">
